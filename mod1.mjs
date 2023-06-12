fac2c2eb4f2bebf367bc30223c42e844878bf5ea
Souvik Maity <souvikmaity599@gmail.com>
2023-06-13 00:51:15 +0530

    Props and propsTypes

---
+++ mod1.mjs
@@ -0,0 +1,4 @@
+import ex,{a, c, d, e} from "./mod2.mjs";
+
+console.log(ex);
+console.log(e);
---
+++ mod2.mjs
@@ -0,0 +1,11 @@
+let a = "Souvik";
+let b = "Pintu";
+let c = "Shyama";
+let d = "Subrata";
+let e = "Suman";
+
+export default b;
+export {a};
+export {c};
+export {d};
+export {e};
